c4dcd5d6953e1195e8255c580f9e355b25e8e3d1
Andrew <rostangeles1993@gmail.com>
2023-05-16 22:41:05 +0300

    commit

--- form.php
+++ form.php
@@ -1,15 +1,17 @@
 <?php
 
-if (empty($_POST['name']) or empty($_POST['age'])) { 
-// В этой строчке показывает, если эти значения пустые, выводит exit
-    exit ('Одно либо два поля заполнены');
-} else {
-    $name = htmlspecialchars($_POST['name']);
-    $age = htmlspecialchars($_POST['age']);
-    // Если в одном из них есть значение, выводит true
-}
+echo "Hello, {$_SESSION['name']}";
 
-echo $name . " " . $age;
+//if (empty($_POST['name']) or empty($_POST['age'])) { 
+//// В этой строчке показывает, если эти значения пустые, выводит exit
+//    exit ('Одно либо два поля заполнены');
+//} else {
+//    $name = htmlspecialchars($_POST['name']);
+//    $age = htmlspecialchars($_POST['age']);
+//    // Если в одном из них есть значение, выводит true
+//}
+
+//echo $name . " " . $age;
 
 /* Если 10 и более полей (анкета), проверяем в empty весь  пост ($_POST),
 без большой проверки */
